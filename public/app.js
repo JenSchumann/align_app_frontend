d2cc689db61c425a61edc40daeb1cf751d20698e
Jen Schumann <jenniferschumann1@gmail.com>
2017-09-15 21:10:30 -0600

    frontend server running

---
+++ public/app.js
@@ -0,0 +1,66 @@
+console.log('Align now with your future');
+
+const app = angular.module('align_app', []);
+
+//main controller
+app.controller('mainController', ['$http',
+  function($http){
+    this.test = "Align now with your future"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////
+}]); //end mainController
