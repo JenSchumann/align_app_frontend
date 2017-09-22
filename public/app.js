395a55bde55a6e9f91e0468fc74600a662aaec31
Jen Schumann <jenniferschumann1@gmail.com>
2017-09-22 15:07:17 -0600

    changed to heroku url

--- public/app.js
+++ public/app.js
@@ -9,8 +9,8 @@ app.controller('mainController', ['$http',
   function($http){
     // this.test = "Align now with your future"
     const controller = this;
-    this.url = 'http://localhost:3000';
-    // this.url = 'https://alignapi.herokuapp.com';
+    // this.url = 'http://localhost:3000';
+    this.url = 'https://alignapi.herokuapp.com';
     this.user = {};
     this.users = [];
     this.userPass = {};
